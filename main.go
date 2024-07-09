6460aaa1cc155759c62f52e28b7437e5cf55d7dd
softwareabbai7 <ktvramanujam@gmail.com>
2024-07-09 22:34:13 +0530

    modified:   main.go

--- main.go
+++ main.go
@@ -1,21 +1,37 @@
 package main
 
 import (
-    "encoding/json"
-    "net/http"
-    "github.com/gorilla/mux"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+type Response struct {
+	Message string `json:"message"`
+}
+
+func greetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	name := vars["name"]
+	message := fmt.Sprintf("%s, hello! How are you?", name)
+
+	response := Response{Message: message}
+	json.NewEncoder(w).Encode(response)
+}
+
 func PingHandler(w http.ResponseWriter, r *http.Request) {
-    response := map[string]string{"response": "pong"}
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(response)
+	response := map[string]string{"response": "pong"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
 
 func main() {
-    r := mux.NewRouter()
-    r.HandleFunc("/ping", PingHandler).Methods("GET")
+	r := mux.NewRouter()
+	r.HandleFunc("/ping", PingHandler).Methods("GET")
+	r.HandleFunc("/greet/{name}", greetHandler).Methods("GET")
 
-    http.Handle("/", r)
-    http.ListenAndServe(":8000", nil)
+	http.Handle("/", r)
+	http.ListenAndServe(":8000", nil)
 }
